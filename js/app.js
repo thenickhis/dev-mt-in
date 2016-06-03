3d7221a850386359bcca6537299306d78f84426f
thenickhis <thenickhis@gmail.com>
2016-06-02 18:52:33 -0600

    first

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$(".nav-button").hover(function (){
+        $(this).css("text-decoration", "underline");
+    },function(){
+        $(this).css("text-decoration", "none");
+    };
+});
